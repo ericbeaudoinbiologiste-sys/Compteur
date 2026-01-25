a0a9fd9bcb67d2d4f4cd4378bb052b33948b87b1
Éric Beaudoin <eric.beaudoin.biologiste@gmail.com>
2026-01-25 09:52:41 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — PWA cache versionné
-const VERSION = "v12.8";                 // <-- incrémente quand tu déploies
+const VERSION = "v12.81";                 // <-- incrémente quand tu déploies
 const CACHE = `timer-${VERSION}`;
 
 const ASSETS = [
