cdf09b2414a5d5be8090eb870a0abaa248e3c1af
Éric Beaudoin <eric.beaudoin.biologiste@gmail.com>
2026-02-01 15:37:23 -0500

    Fix playbeep function calls in app.js

--- app.js
+++ app.js
@@ -1545,8 +1545,8 @@ function finish() {
   timerId = null;
 
   initAudio();
-  playbeep({0.6});
-  setTimeout(() => playbeep({ volume: 0.6,}), 180);
+  playBeep({0.6});
+  setTimeout(() => playBeep({0.6,}), 180);
 
   // On garde la session "affichée", mais on met en pause
   isRunning = true;
