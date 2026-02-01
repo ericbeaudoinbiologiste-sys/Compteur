8cace8c74de9c1085ecd06c37d89124f3195e074
Éric Beaudoin <eric.beaudoin.biologiste@gmail.com>
2026-02-01 15:33:08 -0500

    Increment cache version to v12.82

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — PWA cache versionné
-const VERSION = "v12.81";                 // <-- incrémente quand tu déploies
+const VERSION = "v12.82";
 const CACHE = `timer-${VERSION}`;
 
 const ASSETS = [
